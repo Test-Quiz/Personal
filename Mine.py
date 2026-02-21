d45d359b0498b6f64732e5ac495c5b5e30dbf4b4
FM_Metrics <Gaurav.Gond@eclerx.com>
2026-02-21 20:24:27 +0530

    Update file paths in Mine.py
    Signed-off-by: FM_Metrics <Gaurav.Gond@eclerx.com>

--- Mine.py
+++ Mine.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(
     __name__,
-    template_folder=r"C:\Users\Gaurav.Gond\Desktop\Gaurav\Mine\templates",
-    static_folder=r"C:\Users\Gaurav.Gond\Desktop\Gaurav\Mine\static"
+    template_folder=r"template/index.html",
+    static_folder=r"C:static"
 )
 
 @app.route("/")
@@ -15,10 +15,11 @@ def answer():
     data = request.json
     response = data.get("response")
 
-    with open(r"C:\Users\Gaurav.Gond\Desktop\Gaurav\Mine\responses.txt", "a") as f:
+    with open(r"Personal\responses.txt", "a") as f:
         f.write(response + "\n")
 
     return jsonify({"status": "success"})
 
 if __name__ == "__main__":
     app.run(debug=True)
+
